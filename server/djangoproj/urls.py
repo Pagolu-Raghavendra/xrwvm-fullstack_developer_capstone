68f8cc04cdf6402422d8e32f90d6f2195bca61df
Pagolu-Raghavendra <raghavendramadhu0@gmail.com>
2024-06-07 06:28:15 -0400

    Updating

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,7 +20,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,9 +1,13 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
 import RegisterPanel from "./components/Register/Register"
 function App() {
   return (
     <Routes>
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
     </Routes>
